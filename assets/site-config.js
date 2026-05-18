987faf0e4ac0dd61bf83b12f154589107f062485
cicero_ds <ducksangcho@gmail.com>
2026-05-19 00:14:30 +0900

    Update programTitle in site-config.js
    title test

--- assets/site-config.js
+++ assets/site-config.js
@@ -1,5 +1,5 @@
 window.EXERCISE_AI_SITE_CONFIG = {
-  programTitle: "수원교육지원청 지방공무원 역량강화 연수",
+  programTitle: "지방공무원 역량강화 연수",
   appTitle: "AI 실습실",
   pageTitle: "수원 역량강화 AI 실습실",
   description: "수원교육지원청 지방공무원 역량강화 연수용 AI 실습 프롬프트 런처",
